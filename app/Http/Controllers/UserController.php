cff188352d1b7ce9974c7b2efb4ad9b69abe9ce2
dew97-tech <dewmallick97.bd@gmail.com>
2023-03-25 18:12:33 +0600

    Change Password Functionality Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,4 +60,23 @@ class UserController extends Controller
 
         return redirect('/login')->with($notification);
     }
+    public function UserUpdatePassword(Request $request){
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed',
+        ]); 
+        // Check Old Password Validate
+        if(!Hash::check($request->old_password,auth::user()->password)){
+            // dd(auth::user()->password);
+            // dd(!Hash::check($request->old_password,auth::user()->password));
+            return back()->with("error","Old Password Doesn't Match !!");
+            
+        }
+        // Update The New Password
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+ 
+        return back()->with('status',"Password Changed Successfully");
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -45,6 +45,11 @@
                                         aria-selected="true"><i class="fi-rs-user mr-10"></i>Account details</a>
                                 </li>
                                 <li class="nav-item">
+                                    <a class="nav-link" id="change-password-tab" data-bs-toggle="tab"
+                                        href="#change-password" role="tab" aria-controls="change-password"
+                                        aria-selected="true"><i class="fi-rs-user mr-10"></i>Change Password</a>
+                                </li>
+                                <li class="nav-item">
                                     <a class="nav-link" href="{{ route('user.logout') }}"><i
                                             class="fi-rs-sign-out mr-10"></i>Logout</a>
                                 </li>
@@ -232,6 +237,69 @@
                                                         src="{{ (!empty($userData->photo)) ? url('upload/user_images/'.$userData->photo): url('upload/no_image.jpg')}}"
                                                         alt="User" style="width: 100px; height:100px;">
                                                 </div>
+
+                                                <div class="col-md-12">
+                                                    <button type="submit"
+                                                        class="btn btn-fill-out submit font-weight-bold" name="submit"
+                                                        value="Submit">Save Change</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Change Password --}}
+
+                            <div class="tab-pane fade" id="change-password" role="tabpanel"
+                                aria-labelledby="change-password-tab">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5>Change Password</h5>
+                                    </div>
+                                    <div class="card-body">
+                                        <form method="post" action="{{ route('user.update.password') }}">
+                                            @csrf
+                                            {{-- Validation --}}
+                                            @if (session('status'))
+                                            <div class="alert alert-success" role="alert">
+                                                {{ session('status') }}
+                                            </div>
+                                            @elseif (session('error'))
+                                            <div class="alert alert-danger" role="alert">{{ session('error') }}</div>
+                                            @endif
+
+                                            <div class="row">
+                                                <div class="form-group col-md-12">
+                                                    <label>Old Password <span class="required">*</span></label>
+                                                    <input
+                                                        class="form-control @error('old_password') is-invalid @enderror"
+                                                        name="old_password" type="password" placeholder="Old Password"
+                                                        id="current_password" />
+                                                    @error('old_password')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+
+                                                <div class="form-group col-md-12">
+                                                    <label>New Password <span class="required">*</span></label>
+                                                    <input
+                                                        class="form-control @error('new_password') is-invalid @enderror"
+                                                        name="new_password" type="password" placeholder="New Password"
+                                                        id="new_password" />
+                                                    @error('new_password')
+                                                    <span class="text-danger">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+
+                                                <div class="form-group col-md-12">
+                                                    <label>Confirm Password <span class="required">*</span></label>
+                                                    <input class="form-control" name="new_password_confirmation"
+                                                        type="password" placeholder="Confirm New Password"
+                                                        id="new_password_confirmation" />
+                                                </div>
+
+
 
                                                 <div class="col-md-12">
                                                     <button type="submit"
